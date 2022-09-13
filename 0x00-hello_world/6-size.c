cd42306d8f3d2141a2d515b0ca6966572ba3bf8f
Obed Wambugu <sbgaks@gmail.com>
2022-09-13 16:24:41 -0700

    olaalizaa mehnn data types waah

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -23,7 +23,7 @@ int main(void)
 	
 	printf("Size of a long int: %zu byte(s)\n",sizeof(long int));
 	
-	printf("Size of a long long: %zu byte(s)\n",sizeof(long long));
+	printf("Size of a long long int: %zu byte(s)\n",sizeof(long long));
 	
 	printf("Size of a float: %zu byte(s)\n",sizeof(float));
 
