087e01e3e9a915a7cf76ad45ce816c9f10e3edf8
Obed Wambugu <sbgaks@gmail.com>
2022-09-18 14:14:37 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <unstd.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char c[] = "_putchar\n";
+	_putchar(c);
+	return (0);
+}
