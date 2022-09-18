8bba3c88726d2f1f52934db4557ffef034ff7122
Obed Wambugu <sbgaks@gmail.com>
2022-09-18 15:30:52 -0700

    Signs

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_sign - prints sign of number
+ * 
+ * @c: subject of test
+ *
+ * Return: 1 if positiv 0 if zero -1 if negative
+ *
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+		_putchar('+');
+		return (1);
+	else if (n == 0)
+		_putchar('0');
+		return (0);
+	else if (n < 0)
+		_putchar('-');
+		return (-1);
+}
+
