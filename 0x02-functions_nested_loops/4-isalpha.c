04c78453f5131e7e7e41e84372da14a8f63d7f8c
Obed Wambugu <sbgaks@gmail.com>
2022-09-18 15:17:00 -0700

    Alphabet

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - checks for lowercase character
+ * 
+ * @c: subject of test
+ *
+ * Return: 1 if lowercase 0 if otherwise
+ *
+ */
+int _islower(int c)
+{
+	if (c >= 65 && c <= 90) || (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
+
