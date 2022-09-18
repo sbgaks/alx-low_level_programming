fc6db15b8c8fb5f8a91748863eb6f90de1d071ac
Obed Wambugu <sbgaks@gmail.com>
2022-09-18 15:19:30 -0700

    Alphabet

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -10,7 +10,9 @@
  */
 int _islower(int c)
 {
-	if (c >= 65 && c <= 90) || (c >= 97 && c <= 122)
+	if (c >= 65 && c <= 90)
+		return (1);
+	else if (c >= 97 && c <= 122)
 		return (1);
 	else
 		return (0);
