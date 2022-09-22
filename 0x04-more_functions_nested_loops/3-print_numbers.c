beea45a874945ca8c0caa1a17aa213afec99c38a
Obed Wambugu <sbgaks@gmail.com>
2022-09-22 04:41:54 -0700

    Print numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+
+/**
+ *  * print_numbers - prints numbers
+ *   * Return:0
+ *    */
+
+void print_numbers(void)
+{
+int number = 0;
+
+while (number <= 9)
+{
+_putchar(0 + number);
+number++;
+}
+_putchar('\n');
+}
