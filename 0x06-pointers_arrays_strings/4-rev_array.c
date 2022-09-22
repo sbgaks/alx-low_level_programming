c99dd14e6763b7ad7859a7061ef08adefc57cdf7
Obed Wambugu <sbgaks@gmail.com>
2022-09-22 06:32:52 -0700

    four

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * reverse_array - reverses 
+ * @a : pointer
+ * @n : pointer 
+ * Return: no
+ */
+
+void reverse_array(int *a, int n)
+{
+int i;
+
+int j;
+
+int tmp;
+
+i = 0;
+
+j = n - 1;
+
+while (i < j)
+{
+tmp = a[i];
+a[i] = a[j];
+a[j] = tmp;
+i++;
+j--;
+}
+}
