1bc79948494f33a7e44963ae30039b33c3d24035
Obed Wambugu <sbgaks@gmail.com>
2022-09-22 05:09:51 -0700

    most numbers

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -10,11 +10,11 @@ void print_most_numbers(void)
 char n = 0;
 while (n <= 9)
 {
-if (n != 2 && n != 4 )
-{
-_putchar('0' + n);
-}
-n++;
+	if (n != 2 && n != 4 )
+	{
+	_putchar('0' + n);
+	}
+	n++;
 }
 _putchar('\n');
 }
