9631ccec871ac1580b648b56812dc1d89ccd76ca
Obed Wambugu <sbgaks@gmail.com>
2022-09-22 04:33:01 -0700

    Multipication

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * mul - returns the product of its parameters
+ * @a: first integer
+ * @b: second integer
+ * Return:0
+ */
+
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
