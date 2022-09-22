0c191baae1776b1931c5e2a2c26a5665d8314129
Obed Wambugu <sbgaks@gmail.com>
2022-09-22 05:20:22 -0700

    more numbers

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints more numbers
+ * Return: void
+ */
+void more_numbers(void)
+{
+int number;
+
+for (number == 0; number <= 14; number++)
+{
+	_putchar(number + 0)
+}
+}
